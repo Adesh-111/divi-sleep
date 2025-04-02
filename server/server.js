0f788db7adbd760b4a8fb30f3d064b16bce35f05
Adesh-111 <adeshabinesh25@gmail.com>
2025-04-03 00:10:15 +0530

    change in cors

--- server/server.js
+++ server/server.js
@@ -1,22 +1,23 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import dotenv from "dotenv";
-import sleepRoutes from "./router.js";
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import sleepRoutes from './router.js';
 
 dotenv.config();
 
 const app = express();
 
+const allowedOrigins = ['http://localhost:5173', 'https://divi-sleep.vercel.app/'];
 const corsOptions = {
-  origin: 'https://divi-sleep-api.vercel.app',
-  optionsSuccessStatus: 200 
+  origin: allowedOrigins,
+  optionsSuccessStatus: 200, 
 };
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.use("/api", sleepRoutes);
+app.use('/api', sleepRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
