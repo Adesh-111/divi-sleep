5771af6b80ea73343600213ca8f0a6ebccdbd894
Adesh-111 <adeshabinesh25@gmail.com>
2025-04-03 00:35:58 +0530

    last commit for backend api deployment

--- server/server.js
+++ server/server.js
@@ -1,25 +1,17 @@
 import express from "express";
 import cors from "cors";
-import userRoutes from "./router.js"; 
+import router from "./router.js"; 
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:5173', 'https://divi-sleep.vercel.app/'];
-
-
 app.use(cors({
-  origin: allowedOrigins, 
+  origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true
 }));
 
 app.use(express.json());
-app.use("/api", userRoutes); 
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
+app.use("/api", router); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
