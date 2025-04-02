415eee29a0f9706b8ad76e0191c84c7b6678204c
Adesh-111 <adeshabinesh25@gmail.com>
2025-04-03 00:26:36 +0530

    Change of vercel json

--- server/server.js
+++ server/server.js
@@ -1,23 +1,27 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import sleepRoutes from './router.js';
-
-dotenv.config();
+import express from "express";
+import cors from "cors";
+import userRoutes from "./router.js"; 
 
 const app = express();
 
 const allowedOrigins = ['http://localhost:5173', 'https://divi-sleep.vercel.app/'];
-const corsOptions = {
-  origin: allowedOrigins,
-  optionsSuccessStatus: 200, 
-};
 
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
 
-app.use('/', sleepRoutes);
+app.use(cors({
+  origin: allowedOrigins, 
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true
+}));
+
+app.use(express.json());
+app.use("/api", userRoutes); 
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
